b064a7d48c2a053f71a6ec8a03ffee7a8b903400
Gabriel Sanna <gabrielsanna1@gmail.com>
2017-11-15 19:52:53 -0500

    Add dates to created_at and updated_at fields

--- database/seeds/HighScoresTableSeeder.php
+++ database/seeds/HighScoresTableSeeder.php
@@ -17,6 +17,8 @@ class HighScoresTableSeeder extends Seeder
         for ($i = 1; $i <= 10; $i++) 
         {
             DB::table('high_scores')->insert([
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
                 'name' => $honorifics[rand(0, 5)].' '.str_random(rand(5, 12)),
                 'class' => $classes[rand(0, 2)],
                 'score' => rand(0, 200),
