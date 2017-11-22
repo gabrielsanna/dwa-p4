5bde1c285b378413009a6cec53841043d4cbaf92
Gabriel Sanna <gabrielsanna1@gmail.com>
2017-11-22 15:14:52 -0500

    Tweaks to the highscores page

--- resources/views/GregQuest/highscores.blade.php
+++ resources/views/GregQuest/highscores.blade.php
@@ -5,9 +5,15 @@
         <h3>High Scores</h3>
 
         <table class="table">
-        @foreach($highScores as $indexKey => $score)
             <tr>
-                <td>{{ $indexKey+1 }} </td>
+                <th>Place</th>
+                <th>Player Name</th>
+                <th>Class</th>
+                <th>Score</th>
+            </tr>
+        @foreach($highScores as $key => $score)
+            <tr>
+                <td>{{ $key+1 }} </td>
                 <td>{{ $score["name"] }}</td>
                 <td>{{ $score["class"] }}</td>
                 <td>{{ $score["score"] }}</td>
