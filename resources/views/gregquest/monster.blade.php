6d28ad9e76749991d7520468550729038d532932
Gabriel Sanna <gabrielsanna1@gmail.com>
2017-12-10 13:57:59 -0500

    Tweaks to monster view, and add bootstrap JS

--- resources/views/gregquest/monster.blade.php
+++ resources/views/gregquest/monster.blade.php
@@ -1,10 +1,29 @@
 @extends('gregquest.master')
 
 @section('content')
-    <h3>A Monster Appeared!</h3>
+    <?php 
+#        dump($monster);
+#        dump($adjective);
+    ?>
+
+    <div id="monster_wrapper">
+        <img src="/images/monster.png">
+
+        <h3>A {{ $adjective['adj'] }} {{ $monster['name'] }} appeared!</h3>
+    </div>
 
     <form method='POST'>
         <h3>What would you like to do?</h3>
+
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Select an action...</button>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Kill it!</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </div>
     </form>
 
 @endsection
