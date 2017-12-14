58e5f70ba000e615715caafd1c0c7375c41619ff
Gabriel Sanna <gabrielsanna1@gmail.com>
2017-12-13 20:49:27 -0500

    Make indentation more consistent

--- resources/views/gregquest/master.blade.php
+++ resources/views/gregquest/master.blade.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>GregQuest</title>
-	
-	<meta charset='utf-8'>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css?family=UnifrakturCook:700" rel="stylesheet">
-	<link href="/css/styles.css" type='text/css' rel='stylesheet'>
+    <title>GregQuest</title>
+    
+    <meta charset='utf-8'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=UnifrakturCook:700" rel="stylesheet">
+    <link href="/css/styles.css" type='text/css' rel='stylesheet'>
 </head>
 <body>
-	<div class="container" id="wrapper">
-		<ul class="gabenav">
-			<li class="gabelist"><h1><a href="/">GregQuest</a></h1></li>
-  			<li class="gabelist"><a href="/newgame">New Game</a></li>
-  			<li class="gabelist"><a href="/highscores">High Scores</a></li>
-  			<li class="gabelist"><a href="/encounters">Monsters Fought</a></li>
-		</ul>
+    <div class="container" id="wrapper">
+        <ul class="gabenav">
+            <li class="gabelist"><h1><a href="/">GregQuest</a></h1></li>
+            <li class="gabelist"><a href="/newgame">New Game</a></li>
+            <li class="gabelist"><a href="/highscores">High Scores</a></li>
+            <li class="gabelist"><a href="/encounters">Monsters Fought</a></li>
+        </ul>
 
-		@yield('content')
+        @yield('content')
 
-		@stack('body')
+        @stack('body')
 
-		</div>
+        </div>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
